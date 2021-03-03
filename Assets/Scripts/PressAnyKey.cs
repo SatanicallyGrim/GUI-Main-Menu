1e178795df21ba01fe503df939c52a894222f5ec
SatanicallyGrim <61483076+SatanicallyGrim@users.noreply.github.com>
2021-03-03 22:05:18 +1100

    Menu System
    -Added Keybinds
    -Added Press Any Key Screen
    -Cleaned Up UI
    -Made Shitty Programmer art (cursor)

---
+++ Assets/Scripts/PressAnyKey.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressAnyKey : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+}
